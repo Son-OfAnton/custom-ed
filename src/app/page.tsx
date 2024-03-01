a5155503174ba9c313055629ea14d0a28423a318
deborah <deborahberhanu644@gmail.com>
2024-03-01 21:35:17 +0300

    fix ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,14 +25,14 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div className="relative overflow-hidden text-primary">
+      <div className="relative overflow-hidden text-primary h-full">
         <div className="md:flex block h-screen w-screen md:justify-center justify-between items-center  md:bg-cover md:bg-center md:bg-no-repeat md:bg-[url(../../public/assets/background-home.svg)]">
           <div className="z-30 h-fill justify-between">
             <NavBar />
 
-            <div className="md:grid md:grid-cols-12 block gap-4 md:mt-8 mt-20 items-center w-fill">
+            <div className="md:grid md:grid-cols-12 block gap-4 md:mt-8 mt-20 items-center">
               <div className="col-span-5 md:ml-20 ml-0 mx-auto">
-                <h1 className="md:text-4xl text-2xl font-bold mb-5 md:ml-0 ml-4 md:pt-0 pt-9">
+                <h1 className="md:text-4xl text-2xl  font-bold mb-5 md:ml-0 ml-4 md:pt-0 pt-9">
                   Discover Your Unique Path to Learning.
                 </h1>
                 <p className="mt-2 md:text-small mb-10 md:ml-0 mx-4">
@@ -62,132 +62,127 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="block md:my-10 w-full">
+      <div className="block md:mb-10 md:mt-40 mt-1 w-full">
         <h1 className="text-4xl font-bold text-center">Our Services</h1>
       </div>
-      <div className="md:flex block md:justify-center items-center justify-between mb-40">
-        <div className="md:grid md:grid-cols-12 md:gap-4 md:mt-20 mt-10 items-center justify-center">
-          <div className="col-span-12 md:col-span-4 mx-2 md:mb-0 mb-2 flex justify-center ">
-            <Card className="hover:scale-105 hover:z-10">
-              <CardHeader>
-                <CardTitle className="text-2xl">
-                  Personalized Learning
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <CardDescription>
-                  <Image
-                    src={path}
-                    alt="discovery"
-                    width={300}
-                    height={200}
-                    className="md:object-cover object-cover"
-                  />
-                </CardDescription>
-              </CardContent>
-              
-            </Card>
-          </div>
-          <div className="col-span-12 md:col-span-4 mx-2 md:mb-0 mb-7 flex justify-center">
-            <Card className="hover:scale-105 hover:z-10">
-              <CardHeader>
-                <CardTitle className="text-2xl">Classrooms</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <CardDescription>
-                  <Image
-                    src={filemanager}
-                    alt="File Manager"
-                    width={300}
-                    height={300}
-                    className="md:object-cover object-cover"
-                  />
-                </CardDescription>
-              </CardContent>
-              
-            </Card>
-          </div>
-          <div className="col-span-12 md:col-span-4 mx-2 md:mb-0 mb-7 flex justify-center">
-            <Card className="hover:scale-105 hover:z-10">
-              <CardHeader>
-                <CardTitle className="text-2xl">Forums</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <CardDescription>
-                  <Image
-                    src={forum}
-                    alt="Forum"
-                    width={300}
-                    height={300}
-                    className="md:object-cover object-cover"
-                  />
-                </CardDescription>
-              </CardContent>
-             
-            </Card>
-          </div>
+    <div className="md:flex block md:justify-center items-center justify-between mb-40">
+  <div className="md:grid md:grid-cols-12 md:gap-4 md:mt-20 mt-10 items-center justify-center">
+  <div className="col-span-12 md:col-span-4 mx-2 md:mb-4 mb-10 flex justify-center w-full">
+    <Card className="hover:scale-105 hover:z-10 ">
+      <CardHeader>
+        <CardTitle className="text-xl">
+          Adaptive Learning
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>
+          <Image
+            src={path}
+            alt="discovery"
+            width={0}
+            height={0}
+            className="md:object-cover object-cover w-full h-full"
+          />
+        </CardDescription>
+      </CardContent>
+    </Card>
+  </div>
+  <div className="col-span-12 md:col-span-4 mx-2 md:mb-4 mb-10 flex justify-center w-full">
+    <Card className="hover:scale-105 hover:z-10">
+      <CardHeader>
+        <CardTitle className="text-xl">Classrooms</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>
+          <Image
+            src={filemanager}
+            alt="File Manager"
+            width={0}
+            height={0}
+            className="md:object-cover object-cover w-full h-full"
+          />
+        </CardDescription>
+      </CardContent>
+    </Card>
+  </div>
+  <div className="col-span-12 md:col-span-4 mx-2 md:mb-4 mb-10 flex justify-center w-full">
+    <Card className="hover:scale-105 hover:z-10">
+      <CardHeader>
+        <CardTitle className="text-xl">Forums</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>
+          <Image
+            src={forum}
+            alt="Forum"
+            width={0}
+            height={0}
+            className="md:object-cover object-cover w-full h-full"
+          />
+        </CardDescription>
+      </CardContent>
+    </Card>
+  </div>
+  <div className="col-span-12 md:col-span-4 mx-2 md:mb-4 mb-10 flex justify-center w-full">
+    <Card className="hover:scale-105 hover:z-10">
+      <CardHeader>
+        <CardTitle className="text-xl">Announcements</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>
+          <Image
+            src={announcement}
+            alt="Announcements"
+            width={0}
+            height={0}
+            className="md:object-cover object-cover w-full h-full"
+          />
+        </CardDescription>
+      </CardContent>
+    </Card>
+  </div>
+  <div className="col-span-12 md:col-span-4 mx-2 md:mb-4 mb-10 flex justify-center w-full">
+    <Card className="hover:scale-105 hover:z-10">
+      <CardHeader>
+        <CardTitle className="text-xl">Detailed Insight</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>
+          <Image
+            src={analytics}
+            alt="discovery"
+            width={0}
+            height={0}
+            className="md:object-cover object-cover w-full h-full"
+          />
+        </CardDescription>
+      </CardContent>
+    </Card>
+  </div>
+  <div className="col-span-12 md:col-span-4 mx-2 md:mb-4 mb-10 flex justify-center w-full">
+    <Card className="hover:scale-105 hover:z-10">
+      <CardHeader>
+        <CardTitle className="text-xl">Assessments</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>
+          <Image
+            src={assesments}
+            alt="Assessments"
+            width={0}
+            height={0}
+            className="md:object-cover object-cover w-full h-full"
+          />
+        </CardDescription>
+      </CardContent>
+    </Card>
+  </div>
+</div>
 
-          <div className="col-span-12 md:col-span-4 mx-2 md:mb-0 mb-7 flex justify-center">
-            <Card className="hover:scale-105 hover:z-10">
-              <CardHeader>
-                <CardTitle className="text-2xl">Announcements</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <CardDescription>
-                  <Image
-                    src={announcement}
-                    alt="Announcements"
-                    width={300}
-                    height={300}
-                    className="md:object-cover object-cover"
-                  />
-                </CardDescription>
-              </CardContent>
-              
-            </Card>
-          </div>
-          <div className="col-span-12 md:col-span-4 mx-2 md:mb-0 mb-7 flex justify-center">
-            <Card className="hover:scale-105 hover:z-10">
-              <CardHeader>
-                <CardTitle className="text-2xl">
-                  Detailed Insight of your students
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <CardDescription>
-                  <Image
-                    src={analytics}
-                    alt="discovery"
-                    width={300}
-                    height={300}
-                    className="md:object-cover object-cover"
-                  />
-                </CardDescription>
-              </CardContent>
-              
-            </Card>
-          </div>
-          <div className="col-span-12 md:col-span-4 mx-2 md:mb-0 mb-7 flex justify-center">
-            <Card className="hover:scale-105 hover:z-10">
-              <CardHeader>
-                <CardTitle className="text-2xl">Assessments</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <CardDescription>
-                  <Image
-                    src={assesments}
-                    alt="Assessments"
-                    width={300}
-                    height={300}
-                    className="md:object-cover object-cover"
-                  />
-                </CardDescription>
-              </CardContent>
-              
-            </Card>
-          </div>
-        </div>
-      </div>
+</div>
+
+
+      
     </>
   );
 }
