92d7094f228cd82ac697551f94f740835c93d0d1
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2024-03-12 18:59:56 +0300

    feat: Add prettier and eslint config (#4)

--- src/constants/AdminSideBarItems.tsx
+++ src/constants/AdminSideBarItems.tsx
@@ -1,24 +1,23 @@
-import { SideBarItem } from "@/types/SideNavItems";
-import { School, User, UsersRound } from "lucide-react";
-
+import { SideBarItem } from '@/types/SideNavItems'
+import { School, User, UsersRound } from 'lucide-react'
 
 export const AdminSideBarItems: SideBarItem[] = [
-  {
-    id: "1",
-    text: "Classroom",
-    icon: <School size={30} />,
-    path: "/",
-  },
-  {
-    id: "2",
-    text: "Profile",
-    icon: <User size={30} />,
-    path: "/",
-  },
-  {
-    id: "3",
-    text: "Messages",
-    icon: <UsersRound size={30} />,
-    path: "/",
-  },
-];
+	{
+		id: '1',
+		text: 'Classroom',
+		icon: <School size={30} />,
+		path: '/',
+	},
+	{
+		id: '2',
+		text: 'Profile',
+		icon: <User size={30} />,
+		path: '/',
+	},
+	{
+		id: '3',
+		text: 'Messages',
+		icon: <UsersRound size={30} />,
+		path: '/',
+	},
+]
