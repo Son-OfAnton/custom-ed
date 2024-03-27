1ff10acc4e9602535633a12bc41cd75f1eacd3e5
deborah <deborahberhanu644@gmail.com>
2024-03-27 22:55:10 +0300

    fix:amend profile ui

--- src/app/(users)/teacher/Profile/page.tsx
+++ src/app/(users)/teacher/Profile/page.tsx
@@ -1,29 +1,34 @@
 import React from 'react'
 
 import background from '@public/assets/profile_background.svg'
-import { Calendar, Library, Phone } from 'lucide-react'
+import { Calendar, CircleUser, Library, Phone } from 'lucide-react'
 import Image from 'next/image'
 
 import ProfileFields from '@/components/profileFields'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 
 const page = () => {
 	return (
 		<div>
-			<div>
-				<Image
-					src={background}
-					width={0}
-					height={0}
-					alt='background'
-					className='w-full md:pl-40 mb-4 hidden md:block'
-				/>
+			<div className='flex justify-center pt-20 md:pl-40'>
+				<Avatar className='w-40 h-40'>
+					<AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+					<AvatarFallback>CN</AvatarFallback>
+				</Avatar>
 			</div>
+
 			{/* for the picture */}
-			<div></div>
-			<div className='xl:mt-28 lg:mt-14 md:pl-60 mx-4 md:mx-0'>
-				<div className='space-y-10 xl:w-4/12 md:w-8/12 mx-auto md:my-10 my-auto py-40 md:py-0'>
-					<div className='text-3xl font-bold'>Admas Terefe</div>
+
+			<div className='md:pl-60 md:ml-0 ml-6'>
+				<div className='md:flex justify-around w-full mb-8 space-y-8 md:space-y-0 my-14'>
+					<ProfileFields
+						ProfileFieldItems={{
+							icon: <CircleUser />,
+							text: 'Full Name',
+							value: 'Shadman Ahmed',
+						}}
+					/>
 					<ProfileFields
 						ProfileFieldItems={{
 							icon: <Library />,
@@ -31,6 +36,8 @@ const page = () => {
 							value: 'Computer science',
 						}}
 					/>
+				</div>
+				<div className='md:flex justify-around w-full   mb-8 space-y-8 md:space-y-0'>
 					<ProfileFields
 						ProfileFieldItems={{
 							icon: <Calendar />,
@@ -45,9 +52,9 @@ const page = () => {
 							value: '+25197979779',
 						}}
 					/>
-					<div className='flex mt-10 w-full'>
-						<Button className='text-center w-6/12'>Save</Button>
-					</div>
+				</div>
+				<div className='flex justify-center mt-10 w-full'>
+					<Button className='text-center w-6/12'>Save</Button>
 				</div>
 			</div>
 		</div>
--- src/components/profileFields.tsx
+++ src/components/profileFields.tsx
@@ -18,12 +18,18 @@ const ProfileFields = ({ ProfileFieldItems }: profileFieldsProps) => {
 	}
 
 	return (
-		<div className='flex items-center space-x-2'>
-			<span>{ProfileFieldItems.icon}</span>
-			<span className='flex-grow whitespace-nowrap'>
-				{ProfileFieldItems.text}:
-			</span>
-			<Input value={value} onChange={handleChange} />
+		<div className='flex flex-col space-y-3 md:w-5/12 w-11/12 '>
+			<div className='flex items-center space-x-2'>
+				<span>{ProfileFieldItems.icon}</span>
+				<span className='flex-grow whitespace-nowrap'>
+					{ProfileFieldItems.text}:
+				</span>
+			</div>
+			<Input
+				value={value}
+				onChange={handleChange}
+				className='bg-profile_input py-7 rounded-lg'
+			/>
 		</div>
 	)
 }
